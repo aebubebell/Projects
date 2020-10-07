a9a8a76e0b51a1c84157ebafc2a36c982caf6350
aebubebell <72332181+aebubebell@users.noreply.github.com>
2020-10-06 21:53:23 -0400

    Create BHM.js
    add BHM javascript file

---
+++ BHM.js
@@ -0,0 +1,27 @@
+onEvent("play", "click", function(){
+setScreen("screen2");
+});
+onEvent("sourTru", "click", function(){
+setScreen("screen3");
+});
+onEvent("harrietTub", "click", function(){
+setScreen("screen5");
+});
+onEvent("bookT", "click", function(){
+setScreen("screen4");
+});
+onEvent("georgeC", "click", function(){
+setScreen("screen5");
+});
+onEvent("rayCh", "click", function(){
+setScreen("screen6");
+});
+onEvent("stevWond", "click", function(){
+setScreen("screen5");
+});
+onEvent("playAgain", "click", function(){
+setScreen("screen1");
+});
+onEvent("playAgainn", "click", function(){
+setScreen("screen1");
+});
